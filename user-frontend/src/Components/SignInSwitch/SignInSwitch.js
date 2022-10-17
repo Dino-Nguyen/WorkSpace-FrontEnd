4730e1ce177a7e253267d04b4e262e52c337300e
1808T <nguyenanhtu18081998@gmail.com>
2022-10-17 16:46:35 +0700

    hotfix

--- user-frontend/src/Components/SignInSwitch/SignInSwitch.js
+++
@@ -1,9 +0,0 @@
-import { Box, FormControlLabel, Switch } from '@mui/material';
-
-export default function SignInSwitch() {
-  return (
-    <Box>
-      <FormControlLabel label="Remember me" control={<Switch />} />
-    </Box>
-  );
-}
